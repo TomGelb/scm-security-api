41a04d38e16ba72f95bf7cc313a89f2ebcafac8e
TomGelbman <tom26392@gmail.com>
2025-07-20 13:50:16 -0400

    fixed gitleaks cmd

--- src/scanner/gitleaks/gitleaks.service.ts
+++ src/scanner/gitleaks/gitleaks.service.ts
@@ -1,35 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { IScanner } from '../scanner.interface';
+import * as fs from 'fs/promises';
+import * as path from 'path';
 
 @Injectable()
 export class GitleaksService implements IScanner {
     async scanRepository(localPath: string): Promise<string> {
-        // const { spawn } = await import('child_process');
+        const { spawn } = await import('child_process');
         return new Promise((resolve, reject) => {
-            resolve(`Scanning repository at ${localPath} using Gitleaks...`);
-            // const gitleaks = spawn('gitleaks', ['detect', '--source', localPath, '--report-format', 'json'], {
-            //     stdio: ['ignore', 'pipe', 'pipe'],
+            console.log(`Scanning repository at ${localPath} using Gitleaks...`);
+            // const gitleaks = spawn('gitleaks', ['git', localPath, '--report-format=json'], {
             //     shell: false
             // });
+            const gitleaks = spawn('gitleaks', [
+                'git',
+                localPath,
+                `--report-path=${localPath}_results.json`,
+                '--report-format=json',
+                '--no-banner'  // hides the ASCII logo
+            ]);
 
-            // let output = '';
-            // let errorOutput = '';
+            let output = '';
 
-            // gitleaks.stdout.on('data', (data) => {
-            //     output += data.toString();
-            // });
+            gitleaks.stdout.on('data', (data) => {
+                output += data.toString();
+            });
 
-            // gitleaks.stderr.on('data', (data) => {
-            //     errorOutput += data.toString();
-            // });
+            gitleaks.stderr.on('data', (data) => {
+                output += data.toString();
+            });
 
-            // gitleaks.on('close', (code) => {
-            //     if (code === 0) {
-            //         resolve(output);
-            //     } else {
-            //         reject(new Error(`Gitleaks scan failed with code ${code}: ${errorOutput}`));
-            //     }
-            // });
+
+            gitleaks.on('close', async (code) => {
+                if (code === 0 || code === 1) {
+                    console.log(`Gitleaks process found: ${output}`);
+                    try {
+                        const raw = await fs.readFile(`${localPath}_results.json`, 'utf-8');
+                        const findings = JSON.parse(raw);
+                        resolve(findings);
+                    } catch (err) {
+                        console.error('Failed to parse JSON from Gitleaks:', err.message);
+                        reject(err);
+                    }
+                } else {
+                    reject(new Error(`Gitleaks scan failed with code ${code}: ${output}`));
+                }
+            });
         });
     }
 }
